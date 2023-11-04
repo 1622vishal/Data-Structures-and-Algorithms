5d15602b6c2192746146593259c15e81751a735b
Vishal Yadav <vishalyadav82738@gmail.com>
2023-11-04 19:13:34 +0530

    Added Different approach to reverse array

--- Recursion/ReverseArray.cpp
+++ Recursion/ReverseArray.cpp
@@ -33,3 +33,30 @@ int main()
     }
     return 0;
 }
+
+/*Other approach to reverse an array using Recursion and returning that array*/
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> reverse(int i , int j , vector<int> arr)
+{
+    if(i>=j)
+        return arr;
+    swap(arr[i] , arr[j]);
+    return reverse(i+1 , j-1 , arr);
+}
+
+int main()
+{
+    
+    vector<int> arr = {2,7,8,3,9};
+    vector<int> reversed = reverse(0 , arr.size()-1 , arr);
+    for(auto i : reversed)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
+*/
