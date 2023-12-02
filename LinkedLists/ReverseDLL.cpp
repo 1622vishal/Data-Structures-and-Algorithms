9110a38bbbfbd290dc9fd90a2e0a3319a821033f
Vishal Yadav <vishalyadav82738@gmail.com>
2023-12-02 17:43:17 +0530

    Added ReverseDLL.cpp

---
+++ LinkedLists/ReverseDLL.cpp
@@ -0,0 +1,74 @@
+/*----Written by :- Vishal Yadav----*/
+/*----Date :- 02-12-23----*/
+
+/*Program to reverse a doubly linked list*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data1)       //This constructor is for the cases when we don't want to pass the nullptr everytime
+    {
+        data = data1;
+        next = nullptr;
+        prev = nullptr;
+    }
+    Node(int data1 , Node* next1, Node* prev1)
+    {
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+
+Node* arraytoDLL(vector<int> arr)
+{
+    Node* Head = new Node(arr[0]);
+    Node* temp = Head;
+    for(int i=1 ; i<arr.size() ; i++)
+    {
+        Node* newnode = new Node(arr[i]);     //or Node* newnode = new Node(arr[i] , NULL , temp);
+        newnode->prev = temp;
+        temp->next = newnode;
+        temp = temp->next;     //or temp = newnode;
+    }
+    return Head;
+}
+
+Node* reverseDLL(Node* Head)
+{
+    if(Head == NULL || Head->next == NULL)
+        return Head;
+    Node* current = Head;
+    while(current)
+    {
+        Node* temp = current->next;
+        current->next = current->prev;
+        current->prev = temp;
+        
+        if(current->prev == NULL)
+            Head = current;
+        current = current->prev;
+    }
+    return Head;
+}
+
+int main()
+{
+    vector<int> arr = {34, 67, 98, 23, 44};
+    Node* head = arraytoDLL(arr);
+
+    head = reverseDLL(head);
+    Node* temp = head;
+    while(temp)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    return 0;
+}
